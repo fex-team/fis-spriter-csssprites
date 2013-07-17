73a97f84f961ce327f8341822ed4838cd4433952
xiangshouding <xiangshouding@baidu.com>
2013-07-17 11:00:47 +0800

    update background-position获取x，y

--- libs/cssParser.js
+++ libs/cssParser.js
@@ -18,14 +18,20 @@ CssParser.parse = function (content) {
                 position: [0, 0]
             };
             var bg_reg = /(?:\/\*[\s\S]*?(?:\*\/|$))|\b(background(?:-image)?:)([\s\S]*?)\burl\s*\(\s*("(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|[^)}]+)\s*\)([\s\S]*?);?/gi;
-            var bg_pos_reg = /(left|top|bottom|\d+px|0)\s*(top|right|bottom|\d+px|0)(?:\s*;|$)/;
+            var bg_pos_reg = /(left|top|\d+px|0)\s*(top|right|\d+px|0)(?:\s*;|\s*$)/;
             function _parsePosition(m, left, right) {
                 matchFlag = true;
                 var position = [];
                 if (left) {
+                    if (['left', 'top'].indexOf(left) != -1) {
+                        left = 0;
+                    }
                     position[0] = parseFloat(left);
                 }
                 if (right) {
+                    if (['top', 'right'].indexOf(right) != -1) {
+                        right = 0;
+                    }
                     position[1] = parseFloat(right);
                 }
                 BackgroundMap.position = position;
