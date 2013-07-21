553c3c480f8aba656eb820d0a84cea7b8001f14a
xiangshouding <xiangshouding@baidu.com>
2013-07-21 17:29:59 +0800

    update

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ var pack = require('./libs/pack.js');
 var cssParser = require('./libs/cssParser.js');
 var imgGen = require('./libs/imageGenerate.js');
 
-function isEmpty(obj) {
-    for (var key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            return false;
-        }
-    }
-    return true;
-}
-
 var CssSprite = module.exports = function(ret, conf, settings, opt) {
     //对打包后的css做csssprite
     fis.util.map(ret.pkg, function (subpath, file) {
