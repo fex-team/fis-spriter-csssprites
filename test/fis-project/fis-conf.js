94be389f4e84bdc9cde6f0c5b4a4773fea48d5e1
xiangshouding <xiangshouding@baidu.com>
2013-07-29 14:21:13 +0800

    调整单测

--- test/fis-project/fis-conf.js
+++ test/fis-project/fis-conf.js
@@ -1,11 +1,25 @@
-
 fis.config.merge({
     namespace: 'csssprites',
     modules: {
         spriter: 'csssprites'
     },
+    roadmap: {
+        path: [
+            {
+                reg: '/style/**.css',
+                useSprite: true
+            }
+        ]
+    },
     pack: {
         '/aio.css': '**.css'
+    },
+    settings: {
+        spriter: {
+            csssprites: {
+                margin: 10
+            }
+        }
     }
 });
 
