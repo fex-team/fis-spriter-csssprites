b9b51e8e6451868bc106c4236a20eb341bac08ab
xiangshouding <fansekey@gmail.com>
2013-08-13 12:22:42 +0800

    增强报错机制

--- index.js
+++ index.js
@@ -4,24 +4,32 @@
  * @type {*}
  */
 var cssParser = require('./libs/cssParser.js');
-var imgGen = require('./libs/image.js');
+try {
+    var imgGen = require('./libs/image.js');
+} catch (e) {
+    fis.log.warning('csssprites does not support your node ' + process.version +
+        ', report it to https://github.com/xiangshouding/fis-spriter-csssprites/issues');
+}
 
 module.exports = function(ret, conf, settings, opt) {
+    if (!imgGen) {
+        return;
+    }
     //文件属性中useSprite == true的css做图片合并
     fis.util.map(ret.src, function(subpath, file) {
         if (file.rExt == '.css' && file.useSprite) {
-            process(file, ret, settings, opt);
+            _process(file, ret, settings, opt);
         }
     });
     //打包后的css文件做图片合并
     fis.util.map(ret.pkg, function (subpath, file) {
         if (file.rExt == '.css') {
-            process(file, ret, settings, opt);
+            _process(file, ret, settings, opt);
         }
     });
 };
 
-function process(file, ret, settings, opt) {
+function _process(file, ret, settings, opt) {
     var res = cssParser(file.getContent());
     content = res.content;
     if (res.map && res.map.length > 0) {
